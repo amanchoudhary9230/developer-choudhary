e532913e24a80d6c39a453e50bb9156d121cf1ee
amanchoudhary9230 <amanjaat7668@gmail.com>
2024-06-09 13:31:28 +0530

    add background in footer icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,13 +5,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full text-sm mb-2 md:font-normal flex flex-wrap-reverse justify-between sm:gap-10 gap-4 items-center font-light dark:text-stone-300 text-black-100">
+    <footer className="w-full text-sm mb-2 max-sm:mx-[2px] md:font-normal flex flex-wrap-reverse justify-between sm:gap-10 gap-4 items-center font-light dark:text-stone-300 text-black-100">
       <span>Copyright &copy; 2024 Developer Choudhary</span>
       <span className="flex justify-center gap-4 items-center">
         {socialMedia.map((profile) => (
           <span
             key={profile.id}
-            className="cursor-pointer flex justify-center items-center"
+            className="cursor-pointer flex justify-center bg-black dark:bg-transparent bg-clip-content h-[17px] w-[17px] items-center"
             onClick={() => window.open(profile.src, "_blank")}
           >
             <Image
