14e0b4b2538e7d27b618d99ea09f431586a4cf48
amanchoudhary9230 <amanjaat7668@gmail.com>
2024-06-18 08:59:39 +0530

    update app/layout.tsx using head instead of next/head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { ThemeProvider } from "../context/theme-provider";
-import Head from "next/head";
 
 export const metadata = {
   title: "Developer Choudhary",
@@ -14,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
+      <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="google-adsense-account" content="ca-pub-3643130730451957" />
@@ -23,7 +22,7 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3643130730451957"
           crossOrigin="anonymous"
         ></script>
-      </Head>
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
